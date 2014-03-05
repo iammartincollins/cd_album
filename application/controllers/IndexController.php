f67361994ddbf4c8ba1c163287904d629d003a46
iammartincollins <hi@iammartincollins.com>
2014-03-05 12:00:43 +0000

    Can now edit

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,12 +16,51 @@ class IndexController extends Zend_Controller_Action
 
     public function addAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+        $form->submit->setLabel('Add');
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+                $albums = new Application_Model_DbTable_Albums();
+                $albums->addAlbum($artist, $title);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        }
     }
 
     public function editAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+        $form->submit->setLabel('Save');
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            if ($form->isValid($formData)) {
+                $id = (int)$form->getValue('id');
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+                $albums = new Application_Model_DbTable_Albums();
+                $albums->updateAlbum($id, $artist, $title);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        } else {
+            $id = $this->_getParam('id', 0);
+            if ($id > 0) {
+                $albums = new Application_Model_DbTable_Albums();
+                $form->populate($albums->getAlbum($id));
+            }
+        }
     }
 
     public function deleteAction()
